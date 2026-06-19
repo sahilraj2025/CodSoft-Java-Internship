6a0a2162bbf1618d3eb4d7819a2e2cd3636a12d1
Sahil Raj <sr905418@gmail.com>
2026-06-19 17:52:50 +0530

    Add files via upload

---
+++ Task-2 StudentGradeCalculator.java
@@ -0,0 +1,44 @@
+package codsoft;
+import java.util.Scanner;
+
+public class StudentGradeCalculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of subjects: ");
+        int subjects = sc.nextInt();
+        int totalMarks = 0;
+        for (int i = 1; i <= subjects; i++) {
+            System.out.print("Enter marks for Subject " + i + " (out of 100): ");
+            int marks = sc.nextInt();
+            if (marks < 0 || marks > 100) {
+                System.out.println("Invalid marks! Please enter marks between 0 and 100.");
+                i--;
+                continue;
+            }
+            totalMarks += marks;
+        }
+        double averagePercentage = (double) totalMarks / subjects;
+        String grade;
+
+        if (averagePercentage >= 90) {
+            grade = "A+";
+        } else if (averagePercentage >= 80) {
+            grade = "A";
+        } else if (averagePercentage >= 70) {
+            grade = "B";
+        } else if (averagePercentage >= 60) {
+            grade = "C";
+        } else if (averagePercentage >= 50) {
+            grade = "D";
+        } else {
+            grade = "F";
+        }
+
+        System.out.println("\n----- RESULT -----");
+        System.out.println("Total Marks: " + totalMarks);
+        System.out.println("Average Percentage: " + averagePercentage + "%");
+        System.out.println("Grade: " + grade);
+
+        sc.close();
+    }
+}
